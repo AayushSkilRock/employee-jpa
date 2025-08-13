6edf2193b2c074b79831a1e5c06397db20998da0
Aayush <aayush.gutpta@skilrock.com>
2025-08-13 16:52:17 +0530

    Inital Commit

---
+++ src/test/java/com/employee/employeeJPA/EmployeeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.employeeJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
